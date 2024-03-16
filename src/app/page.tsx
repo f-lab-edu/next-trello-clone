0d177c74a6bbbf4bfcf4471066d91bd588f2459d
Lajancia <50996139+Lajancia@users.noreply.github.com>
2024-03-16 19:33:34 +0900

    #5 [refact] : 드래그 앤 드롭 변수 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
 "use client";
 
+import { useEffect } from "react";
 import DragDrop from "@/components/DragDrop";
+import { useDragStore } from "@/stores/useDragStore";
 
-// component
+import { todoList } from "@/utils/todoList";
+import { useMutation } from "react-query";
+
 const Home = () => {
+  const { todos, lists, setTodos, setLists } = useDragStore();
+  const initialMutation = useMutation(todoList, {
+    onSuccess: (data) => {
+      setTodos(data.todos);
+      setLists(data.lists);
+      console.log(data);
+    },
+  });
+  useEffect(() => {
+    initialMutation.mutate();
+  }, []);
+
   return (
     <div>
       {/* 외부 컴포넌트 적용 */}
-      <DragDrop />
+      <DragDrop
+        todos={todos}
+        lists={lists}
+        setTodos={setTodos}
+        setLists={setLists}
+      />
     </div>
   );
 };
