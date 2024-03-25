2b82ae1f72f3b90c109a061c5214c7f9847b0da2
Lajancia <50996139+Lajancia@users.noreply.github.com>
2024-03-25 22:43:26 +0900

    [fix] : infiniteQuery 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
 "use client";
 
+<<<<<<< HEAD
 import DragDrop from "@/components/DragDrop";
+=======
+import { useEffect } from "react";
+import DragDrop from "@/app/_TodoListDragAndDrop";
+import { useDragStore } from "@/stores/useDragStore";
+import { useTodoListInfiniteQuery } from "@/api/todoList";
+import { useInfiniteQuery } from "react-query";
+import { useInView } from "react-intersection-observer";
+>>>>>>> 04a0201 ([refact] : infiniteQuery 수정)
 
 // component
 const Home = () => {
+<<<<<<< HEAD
+=======
+  const { todos, lists, setTodos, setLists } = useDragStore();
+  const [ref, inView] = useInView();
+
+  const { data, fetchNextPage, hasNextPage, isLoading, isError, refetch } =
+    useTodoListInfiniteQuery();
+
+  useEffect(() => {
+    if (inView) {
+      // useInfiniteQuery enabled:false 해제
+      refetch();
+      if (hasNextPage) {
+        fetchNextPage();
+        if (data) {
+          const allTodos = data.pages.flatMap((page) => page.todos);
+          setTodos(allTodos);
+
+          const allLists = data.pages.flatMap((page) => page.lists);
+          setLists(allLists);
+        }
+      }
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>An error has occurred</div>;
+  }
+
+>>>>>>> 04a0201 ([refact] : infiniteQuery 수정)
   return (
     <div>
       {/* 외부 컴포넌트 적용 */}
